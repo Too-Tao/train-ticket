796e1df81cfff8faf69c0f50625b907773050138
Too-Tao <599366020@qq.com>
2019-07-11 10:13:11 +0800

    feat: 添加 .gitignore

--- train-server/mockData/mock.js
+++ train-server/mockData/mock.js
@@ -1,3 +1,19 @@
 module.exports = {
-  'GET /rest/cities': require('./rest/cities.json')
+  'GET /rest/cities': require('./rest/cities.json'),
+  'GET /rest/search'(req, res) {
+    const { key } = req.query;
+    return res.json({
+        result: [{
+            key: '芜湖',
+            display: '芜湖'
+        }, {
+            key: '井冈山',
+            display: '井冈山',
+        }, {
+            key: '铁岭',
+            display: '铁岭',
+        }],
+        searchKey: key,
+    });
+  },
 }
